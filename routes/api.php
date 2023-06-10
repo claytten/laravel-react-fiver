d0bb865f326a1cd002c16d9a6225a3513aa74f4f
aji <wahyuajisulaiman@gmail.com>
2023-06-10 10:56:36 +0700

    adding throttling on for handling too many request

--- routes/api.php
+++ routes/api.php
@@ -16,16 +16,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('login', [AuthController::class, 'login'])->name('login');
-Route::post('register', [AuthController::class, 'register'])->name('register');
+Route::group(['middleware' => ['throttle:10,1']], function () {
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('register', [AuthController::class, 'register'])->name('register');
+    Route::post('password/email', [ForgetPasswordController::class, 'forgotPassword'])->name('custom.resetpassword.email');
+});
+
 Route::get('email/verify/{id}/{hash}', [VerifyController::class, 'verify'])->middleware('signed')->name('verification.verify');
-Route::post('password/email', [ForgetPasswordController::class, 'forgotPassword'])->name('custom.resetpassword.email');
 Route::post('password/reset', [ForgetPasswordController::class, 'resetPassword'])->name('custom.resetpassword.reset');
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('email/resend', [VerifyController::class, 'resend'])->name('verification.resend');
     Route::post('/logout', [AuthController::class,'logout'])->name('logout');
-    Route::group(['middleware' => 'verfiedApi'], function () {
+    
+    Route::group(['middleware' => ['verfiedApi', 'throttle:5,1']], function () {
         Route::get('/user/getMe', [AuthController::class,'getMe'])->name('user.me');
     });
 });
